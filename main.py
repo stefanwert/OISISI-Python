f665ad8ffe4923d5c3342cacb74abe7669610577
Petrovic <stefan.petrovic2345@gmail.com>
2020-02-10 17:41:12 +0100

    git commit -m “#parsiran ceo direktorijum”

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
-print("hello")
-print("hello")
+from parserr import Parser
+import  os
+import string
+
+if __name__ == "__main__":
+    p = Parser()
+    p.parse('C:\\Users\\Petrovic\\Desktop\\stefan\\test-skup\\python-2.7.7-docs-html\\about.html')
+
+
+    list_of_files={}
+
+    for(dirpath,dirnames,filenames)in os.walk('C:\\Users\\Petrovic\\Desktop\\stefan\\test-skup'):
+        for filename in filenames:
+            if filename.endswith('.html'):
+                #print(dirpath+filename)
+                p.parse(dirpath+'\\'+filename)
+
+    for str in p.links:
+        print(str)
+
+
