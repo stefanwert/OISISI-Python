7d114bdaf228544525a22aae491e342ee5119820
Petrovic <stefan.petrovic2345@gmail.com>
2020-02-17 22:11:50 +0100

    git commit -m “#pretraga dokumenta omogucena”

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 from parserr import Parser
 from graph import Graph
+from set import Set
+from trie import Trie
 import  os
+import time
 
 if __name__ == "__main__":
 
-    putanja = input('C:\\Users\\Korisnik DT\\Desktop\\test-skup')
+    putanja = 'C:\\Users\\Petrovic\\Desktop\\stefan\\test-skup'
 
-    pf = []
-    tr = Trie()
-    pf = parseF(putanja)
-    for w in pf:
-        for i in w.words:
-            tr.insert(i, w.putanja)
 
-    list_of_files={}
+
+    #tr = Trie()
+    #pf = parseF(putanja)
+    #for w in pf:
+        #for i in w.words:
+            #tr.insert(i, w.putanja)
     g=Graph()
+    t = Trie()
     for(dirpath,dirnames,filenames)in os.walk('C:\\Users\\Petrovic\\Desktop\\stefan\\test-skup'):
         #print(dirpath,dirnames)
         for filename in filenames:
@@ -25,7 +28,25 @@ if __name__ == "__main__":
                 g.addVertex(dirpath+'\\'+filename)
                 for lin in p.links:
                     g.addEdge(dirpath+'\\'+filename,lin)
+                for word in p.words:
+                    t.insert(word.lower(),dirpath+'\\'+filename)
+    ulaz=input("unesite rec")
+    while ulaz!="kraj":
+        b=t.does_word_exist(ulaz.lower())
+        if b ==True:
+            print("ima")
+        elif  b==False:
+            print("nema")
+        else:
+            for ret in b:
+                #print(ret)
+                print(ret.html," ",ret.num)
+        print("-------------")
+        ulaz = input("unesite rec")
 
-
+    #rec=input("unesite rec")
+    #fajl=input("unesite naziv fajla")
+    print("------------------------")
+    #t.ispis()
     g.printVertexs()
 
--- trie.py
+++ trie.py
@@ -1,9 +1,19 @@
+
+from set import Set
+
 class TrieNode:
     def __init__(self, letter=""):
         self.key = letter
-        self.doc = Set()
+        self.doc ={} #Set()
         self.children = {}
         self.is_end_of_word = False
+        #self.numberOfRepetition=0
+
+class RetrunHtml:
+    def __init__(self,html,num):
+        self.num=num
+        self.html=html
+
 
 
 class Trie:
@@ -12,6 +22,7 @@ class Trie:
         self.dict = {}
 
     def insert(self, word, doc):
+        word.lower()
         curr_node = self.root
         for l in word:
             if l not in curr_node.children:
@@ -20,16 +31,22 @@ class Trie:
             else:
                 curr_node = curr_node.children[l]
         curr_node.is_end_of_word = True
+       # curr_node.numberOfRepetition+=1
 
-        curr_node.doc.add(doc)
-
+        #curr_node.doc.addElement(doc)  #kad ide set sporije je
+        if doc in curr_node.doc.keys():
+            curr_node.doc[doc]+=1
+        else:
+            curr_node.doc[doc]=1
         self.dict.update({word: doc})
 
     def ispis(self):
         for (key, value) in self.dict.items():
             print(key, "::", value)
 
-    def does_word_exist(self, word, pf):
+    def does_word_exist(self, word):
+        word.lower()
+        print(word)
         if word == "":
             return True
         curr_node = self.root
@@ -37,5 +54,8 @@ class Trie:
             if letter not in curr_node.children:
                 return False
             curr_node = curr_node.children[letter]
-
-        return curr_node.doc
+        setRet=Set()
+        for i in curr_node.doc:
+            if curr_node.is_end_of_word:
+                setRet.addElement(RetrunHtml(i,curr_node.doc[i]))
+        return setRet
